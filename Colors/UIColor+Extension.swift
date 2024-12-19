721ec3a0bcc80992e2ebd46ae2085ffe27afa0d9
Doroteya Galbacheva <galbachevva@gmail.com>
2024-12-19 19:44:05 +0200

    new

--- Colors/UIColor+Extension.swift
+++ Colors/UIColor+Extension.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 extension UIColor {
-    
+    //new
     static func random() -> UIColor {
         let randomColor = UIColor(red: CGFloat.random(in: 0...1),
                                   green: CGFloat.random(in: 0...1),
